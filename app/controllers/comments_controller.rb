1b68f2492170a8d68dd7c3d6a7bbe4a79647cac3
dgirty <dggirty@gmail.com>
2019-08-27 23:46:02 +0000

    static-2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,9 @@
 class CommentsController < ApplicationController
-  def create
-    @place = Place.find(params[:place_id])
-    @place.comments.create(comment_params.merge(user: current_user))
-    redirect_to place_path(@place)
+  def index
+    @comments = Comment.all
   end
 
-  private
-
-  def comment_params
-    params.require(:comment).permit(:message, :rating)
+  def new
+    @comment = Comment.new
   end
 end
-
